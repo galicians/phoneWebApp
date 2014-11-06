243e81873e9ff32836eb9a35f8d748c6b5307309
galicians <galicians@gmail.com>
2014-11-06 11:46:58 +0000

    end to end test added, getting error

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -8,23 +8,34 @@ describe('PhoneCat App', function() {
   describe('Phone list view', function() {
 
     beforeEach(function() {
-      browser.get('app/index.html');
-    });
+      browser.get('app/index.html')
+    })
 
 
     it('should filter the phone list as a user types into the search box', function() {
 
-      var phoneList = element.all(by.repeater('phone in phones'));
-      var query = element(by.model('query'));
+      var phoneList = element.all(by.repeater('phone in phones'))
+      var query = element(by.model('query'))
 
-      expect(phoneList.count()).toBe(3);
+      expect(phoneList.count()).toBe(3)
+
+      query.sendKeys('nexus')
+      expect(phoneList.count()).toBe(1)
+
+      query.clear()
+      query.sendKeys('motorola')
+      expect(phoneList.count()).toBe(2)
+    })
+
+    it('should displey the user input in the title instantly', function() {
+      
+      query.clear();
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
 
       query.sendKeys('nexus');
-      expect(phoneList.count()).toBe(1);
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/); 
 
-      query.clear();
-      query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(2);
-    });
-  });
-});
+    })
+    
+  })
+})
