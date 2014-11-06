9899f2d4a00867544c0586a3303ab52da457d671
galicians <galicians@gmail.com>
2014-11-06 16:26:28 +0000

    all tests green

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,8 +2,7 @@
 
 var phonecatApp = angular.module('phonecatApp', [])
 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', 
-  function($scope, $http) {
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('phones/phones.json').success(function(data) {
       $scope.phones = data
     })
