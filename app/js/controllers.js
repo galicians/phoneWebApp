d830ab466867cabc229206f04de4d8ccb60282ba
galicians <galicians@gmail.com>
2014-11-07 20:20:44 +0000

    redoing from here

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var phonecatApp = angular.module('phonecatApp', [])
+var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('phones/phones.json').success(function(data) {
-      $scope.phones = data
-    })
+phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
+  $http.get('phones/phones.json').success(function(data) {
+    $scope.phones = data;
+  });
 
-  $scope.orderProp = 'age'
-}])
+  $scope.orderProp = 'age';
+});
